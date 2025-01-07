164a684ccc0d6672d9599a835f5e22572618bfde
Erik Assum <erik.assum@ardoq.com>
2025-01-07 06:41:09 +0100

    First episode. I can haz a video

---
+++ src/cms0/main.clj
@@ -0,0 +1,36 @@
+(ns cms0.main
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.util.response :as response]))
+
+(defn ->content []
+  (-> (response/file-response "resources/public/foo.mov")
+      (response/content-type "video/quicktime")))
+
+(defn show-content []
+  (-> "<html><body><video src = \"/content/1\" controls=\"true\"></video></body></html>"
+      (response/response)
+      (response/content-type "text/html")))
+
+(defn list-content []
+  (-> "<html><body><ul><li><a href=\"/show-content/1\">First video</a></li></ul></body></html>"
+      (response/response)
+      (response/content-type "text/html")))
+(defn handler [{:keys [uri] :as req}]
+  (cond
+    (= "/content/1" uri) (->content)
+    (= "/show-content/1" uri) (show-content)
+    :else (list-content)))
+
+(defn start-server [port]
+  (jetty/run-jetty #'handler {:port port :join? false}))
+
+(defn stop-server [server]
+  (.stop server))
+
+(comment
+  (require '[clojure.repl.deps :as deps])
+  (deps/sync-deps)
+
+  (def server (start-server 3000 ))
+  (stop-server server)
+  )
