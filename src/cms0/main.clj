212b0e4d6a7869d9ecaaa5ba3a041ad746e7e798
Erik Assum <erik.assum@ardoq.com>
2025-01-11 12:26:28 +0100

    Second Episode. Hiccup and the first trace of a db

--- src/cms0/main.clj
+++ src/cms0/main.clj
@@ -1,26 +1,45 @@
 (ns cms0.main
   (:require [ring.adapter.jetty :as jetty]
-            [ring.util.response :as response]))
+            [ring.util.response :as response]
+            [hiccup2.core :as hiccup]))
 
-(defn ->content []
-  (-> (response/file-response "resources/public/2025-01-06-20-11-06.mp4")
-      (response/content-type "video/mp4")))
+(defn ->content [{:keys [content-type filename] :as _foo}]
+  (-> (response/file-response filename)
+      (response/content-type content-type)))
 
-(defn show-content []
-  (-> "<!doctype html><html><body><video src = \"/content/1\" controls=\"true\" preload=\"metadata\"></video></body></html>"
+(defn render [h]
+  (-> h
+      (hiccup/html)
+      (str)
       (response/response)
       (response/content-type "text/html")))
 
-(defn list-content []
-  (-> "<!doctype html><html><body><ul><li><a href=\"/show-content/1\">First video</a></li></ul></body></html>"
-      (response/response)
-      (response/content-type "text/html")))
+(defn show-content [{:keys [id] :as _foo}]
+  (-> [:html [:body [:video {:src (str "/content/" id)
+                             :controls true
+                             :preload "metadata"}]]]
+      (render)))
+
+(defn bar [{:keys [id title] :as _foo}]
+  [:li [:a {:href (str "/show-content/" id)} title]])
+
+(defn list-content [foos]
+  (-> [:html [:body [:ul (map bar foos)]]]
+      (render)))
+
+(defonce db (atom [{:content-type "video/mp4"
+                    :filename "resources/public/2025-01-06-20-11-06.mp4"
+                    :title "Episode 1"
+                    :id 1}]))
 
 (defn handler [{:keys [uri] :as req}]
   (cond
-    (= "/content/1" uri) (->content)
-    (= "/show-content/1" uri) (show-content)
-    :else (list-content)))
+    (= "/content/1" uri) (->content (first @db))
+    (= "/show-content/1" uri) (show-content (first @db))
+    (= "/content/2" uri) (->content (second @db))
+    (= "/show-content/2" uri) (show-content (second @db))
+
+    :else (list-content @db)))
 
 (defn start-server [port]
   (jetty/run-jetty #'handler {:port port :join? false}))
@@ -32,6 +51,11 @@
   (require '[clojure.repl.deps :as deps])
   (deps/sync-deps)
 
-  (def server (start-server 3000 ))
+  (def server (start-server 3000))
   (stop-server server)
-  )
+  (swap! db conj {:content-type "video/mp4"
+                  :filename "resources/public/episode-2.mp4"
+                  :title "Episode 2"
+                  :id 2})
+
+  ,)
