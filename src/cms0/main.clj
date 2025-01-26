a0ebd87e8b56c9615e4da8e0f26c6f1fdea4d515
Erik Assum <erik.assum@ardoq.com>
2025-01-26 21:19:46 +0100

    Episode 4: Durable store

--- src/cms0/main.clj
+++ src/cms0/main.clj
@@ -3,7 +3,9 @@
             [ring.util.response :as response]
             [hiccup2.core :as hiccup]
             [ring.middleware.multipart-params :as multipart]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [duratom.core :as duratom]))
 
 (defn ->content [{:keys [content-type filename] :as _foo}]
   (-> (response/file-response filename)
@@ -16,42 +18,46 @@
       (response/response)
       (response/content-type "text/html")))
 
-(defn show-content [{:keys [id] :as _foo}]
-  [:html [:body [:video {:src (str "/content/" id)
-                         :controls true
-                         :preload "metadata"}]]])
+(defn video [{:keys [id] :as _foo}]
+  [:video {:src (str "/content/" id)
+           :controls true
+           :preload "metadata"}])
 
-(defn bar [{:keys [id title] :as _foo}]
-  [:li [:a {:href (str "/show-content/" id)} title]])
-
-(defn list-content [foos]
-  [:html [:body [:ul (map bar foos)]]])
-
-(def upload-form
+(defn page [body]
   [:html
    [:body
-    [:form {:method "post"
-            :action "/upload-content"
-            :enctype "multipart/form-data"}
-     [:input {:type "file"
-              :name "file"}]
-     [:br]
-     [:input {:type "text"
-              :name "title"}]
-     [:br]
-     [:input {:type "submit" :name "Upload"}]
-     ]]])
+    body]])
 
-(defonce db (atom [{:content-type "video/mp4"
-                    :filename "resources/public/2025-01-06-20-11-06.mp4"
-                    :title "Episode 1"
-                    :id 1}]))
+(defn bar [{:keys [id title] :as _foo}]
+  [:li [:a {:href (str "/video/" id)} title]])
+
+(defn home [foos]
+  [:ul (map bar foos)])
+
+(def upload-form
+  [:form {:method "post"
+          :action "/upload-content"
+          :enctype "multipart/form-data"}
+   [:input {:type "file"
+            :name "file"}]
+   [:br]
+   [:input {:type "text"
+            :name "title"}]
+   [:br]
+   [:input {:type "submit" :name "Upload"}]])
+
+(defonce db (duratom/duratom :local-file
+                             :file-path "resources/db"
+                             :init []))
+
+(defn next-id [db]
+  (->> db (map :id) (apply max) inc))
 
 (defn handle-upload [req]
   (let [{:keys [multipart-params] :as req}
         (multipart/multipart-params-request req {})
         file (get multipart-params "file")
-        id (->> @db (map :id) (apply max) inc)
+        id (next-id @db)
         path (str "resources/public/content-" id)
         destination (java.io.File. path)]
     (io/copy (:tempfile file) destination)
@@ -61,20 +67,22 @@
             :filename path
             :id id})))
 
+(defn ->id [uri]
+  (-> uri (str/split #"/") last parse-long))
+
+(defn by-id [db id]
+  (nth db (dec id)))
+
 (defn handler [{:keys [uri] :as req}]
   (cond
-    (= "/content/1" uri) (->content (first @db))
-    (= "/show-content/1" uri) (render (show-content (first @db)))
-    (= "/content/2" uri) (->content (second @db))
-    (= "/show-content/2" uri) (render (show-content (second @db)))
-    (= "/content/3" uri) (->content (nth @db 2))
-    (= "/show-content/3" uri) (render (show-content (nth @db 2)))
+    (str/starts-with? uri "/content/") (->content (by-id @db (->id uri)))
+    (str/starts-with? uri "/video/") (render (page (video (by-id @db (->id uri)))))
 
-    (= "/upload-form" uri) (render upload-form)
- #_#_   (= "/upload-content" uri) (do (handle-upload req)
-                                  (response/redirect "/"))
+    (= "/upload-form" uri) (render (page upload-form))
+    #_#_(= "/upload-content" uri) (do (handle-upload req)
+                                      (response/redirect "/"))
 
-    :else (render (list-content @db))))
+    :else (render (page (home @db)))))
 
 (defn start-server [port]
   (jetty/run-jetty #'handler {:port port :join? false}))
@@ -85,12 +93,11 @@
 (comment
   (require '[clojure.repl.deps :as deps])
   (deps/sync-deps)
-
+  
   (def server (start-server 3000))
   (stop-server server)
   (swap! db conj {:content-type "video/mp4"
                   :filename "resources/public/episode-2.mp4"
                   :title "Episode 2"
                   :id 2})
-
   ,)
