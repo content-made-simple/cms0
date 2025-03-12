b801b42177ae302f80966c3881d1da1c5903972a
Erik Assum <erik.assum@ardoq.com>
2025-03-12 21:52:36 +0100

    Episode 7

--- src/cms0/main.clj
+++ src/cms0/main.clj
@@ -60,9 +60,19 @@
 (def signup-form
   [:form {:method "post"
           :action "/signup"}
+   [:div "email"
+    [:input {:type "text"
+             :name "email"}]]
    [:br]
-   [:input {:type "text"
-            :name "email"}]
+   [:input {:type "submit" :name "Signup"}]])
+
+(def reset-token-form
+  [:form {:method "post"
+          :action "/reset-token"}
+   [:div "email" [:input {:type "text"
+                          :name "email"}]]
+   [:div "token" [:input {:type "text"
+                          :name "token"}]]
    [:br]
    [:input {:type "submit" :name "Signup"}]])
 
@@ -70,12 +80,19 @@
   [:div
    (str "Your token is " token " put it somewhere safe")])
 
+(defn reset-token [user]
+  (if user
+    [:div
+     (str "Your token is " (:token user) " put it somewhere safe")]
+    [:div
+     "You're cheating"]))
+
 (defonce db (duratom/duratom :local-file
                              :file-path "resources/db"
                              :init []))
 
 (defn next-id [db collection]
-  (or (some->> db collection (map :id) seq (apply max) inc)
+  (or (some->> @db collection (map :id) seq (apply max) inc)
       0))
 
 (defn by-email [email user]
@@ -92,7 +109,7 @@
        (by-email email user)))
 
 (defn query [db collection query-fn]
-  (->> db
+  (->> @db
        collection
        (filter query-fn)))
 
@@ -102,23 +119,23 @@
 (defn content-by-id [db id]
   (query-one db :content (partial by-id id)))
 
-(defn transact! [db collection x]
-  (swap! db update collection conj x))
+(defn transact! [db collection f x]
+  (swap! db update collection f x))
 
 (defn handle-upload [req]
   (let [{:keys [multipart-params] :as req} (multipart/multipart-params-request req)
         {:strs [file email token]} multipart-params
-        id (next-id @db :content)
+        id (next-id db :content)
         path (str "resources/public/content-" id)
         destination (java.io.File. path)
         user (query-one @db :user (partial by-token-and-email token email))]
     (if user
       (do (io/copy (:tempfile file) destination)
-          (transact! db :content {:content-type (:content-type file)
-                                  :title (get multipart-params "title")
-                                  :filename path
-                                  :user-id (:id user)
-                                  :id id}))
+          (transact! db :content conj {:content-type (:content-type file)
+                                       :title (get multipart-params "title")
+                                       :filename path
+                                       :user-id (:id user)
+                                       :id id}))
       {::errors {:status 403
                  :body "Unknown user"}})))
 
@@ -140,37 +157,58 @@
       string->digest
       bytes->hexstring))
 
-(defn handle-signup [req]
+(defn handle-signup [{:keys [db timestamp] :as req}]
   (let [{:keys [form-params]} (params/assoc-form-params req "UTF-8")
         email (get form-params "email")
-        id (next-id @db :user)
-        timestamp (inst-ms (java.util.Date.))
+        id (next-id db :user)
         user {:id id
               :email email
               :token (->token [email timestamp])}]
-    (transact! db :user user)
+    (transact! db :user user conj)
     user))
+
+(defn replace [xs to-replace]
+  (mapv (fn [x]
+          (if (= (:id x) (:id to-replace))
+            to-replace
+            x)) xs))
+
+(defn handle-reset-token [{:keys [db timestamp] :as req}]
+  (let [{:keys [form-params]} (params/assoc-form-params req "UTF-8")
+        email (get form-params "email")
+        token (get form-params "token")]
+    (when-let [user (query-one db :user (partial by-token-and-email token email))]
+      (let [new-user (assoc user :token (->token [email timestamp]))]
+        (transact! db :user replace new-user)
+        new-user))))
+
 
 (defn handle-errors [errors]
   errors)
 
 (defn error [{:keys [query-string] :as req}]
   [:h1 query-string])
+
 (defn handler [{:keys [uri] :as req}]
-  (cond
-    (str/starts-with? uri "/content/") (->content (content-by-id @db  (->id uri)))
-    (str/starts-with? uri "/video/") (render (page (video (content-by-id @db  (->id uri)))))
+  (let [req (assoc req
+                   :timestamp (inst-ms (java.util.Date.))
+                   :db db)]
+    (cond
+      (str/starts-with? uri "/content/") (->content (content-by-id db (->id uri)))
+      (str/starts-with? uri "/video/") (render (page (video (content-by-id db (->id uri)))))
 
-    (= "/upload-form" uri) (render (page upload-form))
-    (= "/signup-form" uri) (render (page signup-form))
-    (= "/signup" uri)  (render (page (token (handle-signup req))))
+      (= "/upload-form" uri) (render (page upload-form))
+      (= "/signup-form" uri) (render (page signup-form))
+      (= "/signup" uri)  (render (page (token (handle-signup req))))
+      (= "/reset-token-form" uri) (render (page reset-token-form))
+      (= "/reset-token" uri) (render (page (reset-token (handle-reset-token req))))
 
-#_#_    (= "/upload-content" uri) (let [upload-result (handle-upload req)]
-                                (if-let [errors  (::errors upload-result)]
-                                  (response/redirect-after-post "/error?Unauthenticated")
-                                  (response/redirect-after-post "/")))
-    (= "/error" uri) (render (page (error req)))
-    :else (render (page (home (:content @db))))))
+      (= "/upload-content" uri) (let [upload-result (handle-upload req)]
+                                  (if-let [errors  (::errors upload-result)]
+                                    (response/redirect-after-post "/error?Unauthenticated")
+                                    (response/redirect-after-post "/")))
+      (= "/error" uri) (render (page (error req)))
+      :else (render (page (home (:content @db)))))))
 
 (defn start-server [port]
   (jetty/run-jetty #'handler {:port port :join? false}))
@@ -182,10 +220,11 @@
   (require '[clojure.repl.deps :as deps])
   (deps/sync-deps)
   (:user @db)
+
   (def server (start-server 3000))
   (stop-server server)
   (swap! db conj {:content-type "video/mp4"
                   :filename "resources/public/episode-2.mp4"
-                  :title "Episode 2"
+                  :Title "Episode 2"
                   :id 2})
-  ,)
+  (swap! db assoc-in [:content 5 :content-type] "video/mp4"))
